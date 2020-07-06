69a487529249c95a1c2eb39e8d74d2512641c23f
mdio <michel.dio33@gmail.com>
2020-07-06 16:22:54 +0200

    Init Layout

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import 'styles/index.scss';
+
+import { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export default ({Component, pageProps}) => <Component {...pageProps} />
